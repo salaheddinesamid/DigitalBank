4bde85ca6fde6dbc9ab81f6e05f9b3729a405103
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-13 21:56:15 +0100

    update

--- DigitalBank/settings/dev.py
+++ DigitalBank/settings/dev.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'rest_framework',
     'accounts',
-    'banking'
+    'banking',
+    'user_auth',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
