d51422777f30b337d6d0a3b43a394b771c458e8e
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-13 12:06:32 +0100

    define new account serializer

--- banking/serializers/account_opening_serializer.py
+++ banking/serializers/account_opening_serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from ..models import AccountOpeningRequest
+from ..models import AccountOpeningRequest, BankAccount
 
 
 class AccountOpeningRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountOpeningRequest
+        fields = '__all__'
+
+
+class NewAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
         fields = '__all__'
