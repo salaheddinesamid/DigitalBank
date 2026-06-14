16dc5c5ba49d8674533b2410f0cf21a1235d7ae9
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-14 23:23:28 +0100

    fix the business logic

--- banking/services/withdraw_service.py
+++ banking/services/withdraw_service.py
@@ -4,13 +4,13 @@ from customer_management.exceptions.inactive_account_exception import InactiveBa
 from django.db import transaction
 
 
-def make_withdraw(account_number, amount):
-    if amount < 0:
+def make_withdraw(validate_data):
+    if validate_data['amount'] < 0:
         raise ValueError("Please enter a positive value")
 
     with transaction.atomic():
         bank_account = BankAccount.objects.select_for_update().get(
-            account_number=account_number
+            account_number=validate_data['account_number']
         )
 
         # If the account is inactive, raise an exception
@@ -18,11 +18,11 @@ def make_withdraw(account_number, amount):
             raise InactiveBankAccountException('This account is inactive, please activate your account and try again')
 
         # If the account is out of balance, raise an exception
-        if amount > bank_account.balance:
+        if validate_data['amount'] > bank_account.balance:
             raise InsufficientBalanceException('Insufficient balance, please try again')
 
         # Otherwise, update:
-        bank_account.balance -= amount
+        bank_account.balance -= validate_data['amount']
         bank_account.save()
 
     return bank_account
