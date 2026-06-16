e13e0dbfe7f884912ae748b4e8cadb58fcafbd84
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-16 16:24:41 +0100

    add monthly and daily transfer limit

--- banking/models.py
+++ banking/models.py
@@ -37,6 +37,18 @@ class BankAccount(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
+    daily_limit = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=10000
+    )
+
+    monthly_limit = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=100000
+    )
+
     customer = models.OneToOneField(
         Customer,
         on_delete=models.CASCADE
