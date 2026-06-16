e563e4217f746c7ae7f4722b3d1d1c2e74b7d2c7
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-16 17:40:11 +0100

    add account transactions

--- banking/serializers/customer_account_serializer.py
+++ banking/serializers/customer_account_serializer.py
@@ -1,5 +1,6 @@
+from django.db import models
 from rest_framework import serializers
-from ..models import BankAccount
+from ..models import BankAccount, TransactionRecord
 from customer_management.models import Customer
 
 
@@ -15,10 +16,60 @@ class CustomerBankAccountSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class RecentTransactionSerializer(serializers.ModelSerializer):
+    source_account = serializers.CharField(
+        source="source_account.account_number",
+        read_only=True
+    )
+
+    destination_account = serializers.CharField(
+        source="destination_account.account_number",
+        read_only=True
+    )
+
+    class Meta:
+        model = TransactionRecord
+
+        fields = [
+            "id",
+            "type",
+            "amount",
+            "status",
+            "source_account",
+            "destination_account",
+            "timestamp"
+        ]
+
+
 class CustomerAccountSerializer(serializers.ModelSerializer):
-    customer = CustomerDetailsSerializer(read_only=True)
-    bank_account = CustomerBankAccountSerializer(read_only=True)
+    customer = CustomerDetailsSerializer(
+        read_only=True
+    )
+
+    recent_transactions = serializers.SerializerMethodField()
 
     class Meta:
         model = BankAccount
-        fields = '__all__'
+
+        fields = [
+            "id",
+            "account_number",
+            "type",
+            "balance",
+            "status",
+            "customer",
+            "recent_transactions"
+        ]
+
+    def get_recent_transactions(self, account):
+        transactions = TransactionRecord.objects.filter(
+            models.Q(source_account=account) |
+            models.Q(destination_account=account)
+        ).order_by(
+            "-timestamp"
+        )[:5]
+
+        return RecentTransactionSerializer(
+            transactions,
+            many=True
+        ).data
