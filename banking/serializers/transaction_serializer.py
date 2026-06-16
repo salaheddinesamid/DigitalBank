d022500a2d7b34807566d30166db653b7aadc976
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-06-16 10:53:50 +0100

    update and fix serializer

--- banking/serializers/transaction_serializer.py
+++ banking/serializers/transaction_serializer.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from ..models import TransactionRecord
 
 
 class TransactionSerializer(serializers.Serializer):
     account_number = serializers.CharField()
     amount = serializers.FloatField()
+
+
+class TransactionDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TransactionRecord
+        fields = '__all__'
